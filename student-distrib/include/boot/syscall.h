8cfb3008dff777189dc64b4a7767ffb1ebc056de
Bowen Li <243742619@qq.com>
2022-11-04 17:44:30 -0500

    syscall linkage setup

--- student-distrib/include/boot/syscall.h
+++ student-distrib/include/boot/syscall.h
@@ -7,13 +7,12 @@ void syscall_handler();
 
 
 /* Required by ECE391. */
-
-// int32_t halt(uint8_t status);
-// int32_t execute(const uint8_t *cmd);
-// int32_t open(const uint8_t *filename);
-// int32_t close(int32_t fd);
-// int32_t read(int32_t fd, void *buf, uint32_t nbytes);
-// int32_t write(int32_t fd, const void *buf, uint32_t nbytes);
+int32_t sys_halt(uint8_t status);
+int32_t sys_exe(const uint8_t *cmd);
+int32_t sys_open(const uint8_t *filename);
+int32_t sys_close(int32_t fd);
+int32_t sys_read(int32_t fd, void *buf, uint32_t nbytes);
+int32_t sys_write(int32_t fd, const void *buf, uint32_t nbytes);
 // int32_t getargs(uint8_t *buf, int32_t nbytes);
 // int32_t vidmap(uint8_t **screen_start);
 // int32_t set_handler(int32_t signum, void *handler_addr);
