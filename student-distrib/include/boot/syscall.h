f25884e34f6c3f518f85d79bdf7ceaa46d7d7393
Bowen Li <243742619@qq.com>
2022-11-04 20:13:50 -0500

    add more syscalls

--- student-distrib/include/boot/syscall.h
+++ student-distrib/include/boot/syscall.h
@@ -5,23 +5,21 @@
 
 
 #define SYSCALL 0x80
-// #define asmlinkage __attribute__((regparm(0)))
+#define asmlinkage __attribute__((regparm(0)))
 
 void syscall_handler();
 
-
 /* Required by ECE391. */
- int32_t sys_halt(uint8_t status);
- int32_t sys_exe(const uint8_t *cmd);
- int32_t sys_open(const uint8_t *filename);
- int32_t sys_close(int32_t fd);
- int32_t sys_read(int32_t fd, void *buf, uint32_t nbytes);
- int32_t sys_write(int32_t fd, const void *buf, uint32_t nbytes);
-// int32_t getargs(uint8_t *buf, int32_t nbytes);
-// int32_t vidmap(uint8_t **screen_start);
-// int32_t set_handler(int32_t signum, void *handler_addr);
-// int32_t sigreturn(void);
-
+asmlinkage int32_t sys_halt(uint8_t status);
+asmlinkage int32_t sys_execute(const uint8_t *cmd);
+asmlinkage int32_t sys_open(const uint8_t *filename);
+asmlinkage int32_t sys_close(int32_t fd);
+asmlinkage int32_t sys_read(int32_t fd, void *buf, uint32_t nbytes);
+asmlinkage int32_t sys_write(int32_t fd, const void *buf, uint32_t nbytes);
+asmlinkage int32_t sys_getargs(uint8_t *buf, int32_t nbytes);
+asmlinkage int32_t sys_vidmap(uint8_t **screen_start);
+asmlinkage int32_t sys_set_handler(int32_t signum, void *handler_addr);
+asmlinkage int32_t sys_sigreturn(void);
 
 
 // /* --------------------------------Process-------------------------------- */
--- student-distrib/kernel/syscall.c
+++ student-distrib/kernel/syscall.c
@@ -3,33 +3,44 @@
 
 /**
  * @brief A system call handler.
- * 
+ *
  */
-void do_syscall() {
-    printf("----------------| SYSCALL OCCURED |---------------- \n");
+asmlinkage int32_t sys_halt(uint8_t status) {
+   return 0;
 }
 
-int32_t sys_halt(uint8_t status) {
-    return 0;
+asmlinkage int32_t sys_execute(const uint8_t *cmd) {
+   return 0;
 }
- int32_t sys_exe(const uint8_t *cmd) {
-    return 0;
 
- }
- int32_t sys_open(const uint8_t *filename) {
-    return 0;
+asmlinkage int32_t sys_open(const uint8_t *filename) {
+   return 0;
+}
 
- }
- int32_t sys_close(int32_t fd) {
-        return 0;
+asmlinkage int32_t sys_close(int32_t fd) {
+   return 0;
+}
 
+asmlinkage int32_t sys_read(int32_t fd, void *buf, uint32_t nbytes) {
+   return 0;
+}
 
- }
- int32_t sys_read(int32_t fd, void *buf, uint32_t nbytes) {
-    return 0;
+asmlinkage int32_t sys_write(int32_t fd, const void *buf, uint32_t nbytes) {
+   return 0;
+}
 
- }
- int32_t sys_write(int32_t fd, const void *buf, uint32_t nbytes) {
-    return 0;
+asmlinkage int32_t sys_getargs(uint8_t *buf, int32_t nbytes) {
+   return 0;
+}
 
- }
+asmlinkage int32_t sys_vidmap(uint8_t **screen_start) {
+   return 0;
+}
+
+asmlinkage int32_t sys_set_handler(int32_t signum, void *handler_addr) {
+   return 0;
+}
+
+asmlinkage int32_t sys_sigreturn(void) {
+   return 0;
+}
